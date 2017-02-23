15431f5c95412af7919b389ec73065f58a9dfec2
George Spake <gpspake@gmail.com>
2017-02-22 21:56:25 -0600

    create widget migration

---
+++ database/migrations/2017_02_23_025731_create_widgets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWidgetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('widgets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->decimal('price', 15, 4);
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('widgets');
+    }
+}
